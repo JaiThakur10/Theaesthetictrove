52f77a0ccec4948be65991acb2e8c8a44148131b
JaiThakur10 <dev10jai@gmail.com>
2025-03-27 14:03:54 +0530

    number

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -44,7 +44,7 @@ const Checkout = () => {
     console.log("Placing Order...");
 
     // Open WhatsApp order message
-    const whatsappNumber = "9149724411"; // Replace with your WhatsApp number
+    const whatsappNumber = "7006140255";
     const message = generateOrderMessage();
     const whatsappURL = `https://wa.me/${whatsappNumber}?text=${message}`;
     window.open(whatsappURL, "_blank");
